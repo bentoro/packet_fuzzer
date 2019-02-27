edcb928d428836d759718dd7e11c324dac80d4ee
Ben <bl_04@hotmail.com>
2019-02-26 16:13:57 -0800

    working on queue

--- lib/fuzz.c
+++ lib/fuzz.c
@@ -1,2 +1,58 @@
 #include "fuzz.h"
 
+
+int fuzz(){
+
+}
+int fuzz_payload(){
+}
+struct Queue* create_queue(unsigned capacity){
+    struct Queue* queue = (struct Queue*) malloc(sizeof(struct Queue));
+
+    queue->capacity = capacity;
+    queue->front = 0;
+    queue->size = 0;
+    queue->rear = capacity -1;
+    queue->array = (char **) calloc(queue->capacity, sizeof(char));
+
+    return queue;
+}
+
+void enqueue(struct Queue* queue, char* item){
+    if(is_full(queue)){
+        return;
+    }
+    queue->rear = (queue->rear + 1);
+    queue->array[queue->rear] = item;
+    queue->size = queue->size +1;
+    printf("%s added to queue",item);
+}
+char* dequeue(struct Queue* queue){
+    if(is_empty(queue)){
+        return NULL;
+    }
+    char* item = queue->array[queue->front];
+    queue->front = (queue->front +1);
+    queue->size = queue->size-1;
+    return item;
+}
+int is_full(struct Queue* queue){
+    return(queue->size == (int)queue->capacity);
+}
+int is_empty(struct Queue* queue){
+    return(queue->size == 0);
+}
+
+char* front(struct Queue* queue){
+    if(is_empty(queue)){
+        printf("Queue is empty");
+    }
+    return queue->array[queue->front];
+}
+char* rear(struct Queue* queue){
+    if(is_empty(queue)){
+        printf("Queue is empty");
+    }
+    return queue->array[queue->rear];
+}
+
--- lib/fuzz.h
+++ lib/fuzz.h
@@ -1,8 +1,23 @@
 #ifndef FUZZ_H
 #define FUZZ_H
 
+#include <stdlib.h>
+#include <stdio.h>
+
+struct Queue{
+    int front, rear, size;
+    char **array;
+    unsigned capacity;
+};
+
 int fuzz();
 int fuzz_payload();
-int enque
+struct Queue* create_queue(unsigned capacity);
+void enqueue();
+char* dequeue();
+int is_full(struct Queue* queue);
+int is_empty(struct Queue* queue);
+char* front(struct Queue* queue);
+char* rear(struct Queue* queue);
 
 #endif
